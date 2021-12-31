9fae6122cde367a27782c482592e4e326d6ab9a8
kc244 <kevincho@bwh.harvard.edu>
2021-12-31 09:32:33 -0500

    feat: setup for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='nifti-snapshot',
+    packages=['nifti-snapshot'],
+    version='v0.1.0',
+    description='First release to test pypi',
+    author='kcho',
+    author_email='kevincho@bwh.harvard.edu',
+    url='https://github.com/pnlbwh/nifti-snapshot',
+    download_url='https://github.com/pnlbwh/nifti-snapshot/archive/refs/tags/nifti-snapshot.zip',
+    keywords=['nifti', 'snapshot'],
+    classifiers=[],
+)
